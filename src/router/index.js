2011820ec488f7cbb24f5e5b1069a7f5c1f02ec0
jungsoekoh <63844281+jungsoekoh@users.noreply.github.com>
2021-05-13 15:25:38 +0900

    1. index.js 수정 1) create 컴포넌트의 path params, query params를 받을 수 있게끔 수정.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Read from '@/components/Read.vue'
 import Create from '@/components/Create.vue'
+import Detail from '@/components/Detail.vue'
 
 
 Vue.use(VueRouter)
@@ -13,10 +14,15 @@ const routes = [
     component: Read
   },
   {
-    path: '/create',
+    path: '/create/:contentId?', // path params를 넣은 후부터는 param값이 전달되지 않으면 빈 컴포넌트가 뜬다. 이럴경우 query params를 넣는다.
     name: 'Create',
     component: Create
-  }
+  },
+  {
+    path: '/detail/:contentId',
+    name: 'Detail',
+    component: Detail
+  },
 ]
 
 const router = new VueRouter({
