093d08ac157717e740de82a7e6c40172f35588fa
jungsoekoh <63844281+jungsoekoh@users.noreply.github.com>
2021-05-13 14:03:49 +0900

    1. Create.vue 화면 구성 - input란 2개, textarea 1개 추가 및 양방향 바인딩 - data import 및 data return - write함수 추가   - data(어레이 객체)에 push하도록, /(Read) 화면으로 되돌아가기.

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,12 +1,42 @@
 <template>
     <div>
-        Create
+        <input v-model="writer" placeholder="글쓴이">    
+        <input v-model="title" placeholder="제목">     
+        <textarea v-model="content" placeholder="내용"></textarea>
+        <button @click="write">작성</button>
+        <div>
+            {{writer}}
+            {{title}}
+            {{content}}
+        </div>
     </div>
 </template>
 
 
 <script>
+import data from '@/data'
+
 export default {
-    name: 'Create'
+    name: 'Create',
+    data() {
+        return {
+            data: data,
+            writer: "",
+            title: "",
+            content: "",
+        }
+    },
+    methods: {
+        write() {
+            this.data.push({
+                writer: this.writer,
+                title: this.title,
+                content: this.content
+            })
+            this.$router.push({
+                path: '/'
+            })
+        }
+    }
 }
 </script>
